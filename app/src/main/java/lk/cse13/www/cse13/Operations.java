dc0d9f82dc4c19270acadbe89d231fc93f756f20
kmchmk1026 <kmchmk@gmail.com>
2017-08-27 11:29:59 +0530

    File write function improved.

--- app/src/main/java/lk/cse13/www/cse13/Operations.java
+++ app/src/main/java/lk/cse13/www/cse13/Operations.java
@@ -1,6 +1,7 @@
 package lk.cse13.www.cse13;
 
 import android.content.Context;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,28 +12,17 @@ import java.io.OutputStreamWriter;
 class Operations {
 
     public static String readFromFile(String file) {
-
         String ret = "";
-
         try {
             InputStream inputStream = MainActivity.mainContext.openFileInput(file);
-
             if (inputStream != null) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String receiveString;
-                StringBuilder stringBuilder = new StringBuilder();
-
-                while ((receiveString = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(receiveString);
-                }
-
+                ret = bufferedReader.readLine();
                 inputStream.close();
-                ret = stringBuilder.toString();
             }
         } catch (IOException e) {
         }
-
         return ret;
     }
 
